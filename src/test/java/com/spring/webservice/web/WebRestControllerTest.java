9fda132112f4b85f798e15073bb940bd720eadd5
jamie <rmk1075@gmail.com>
2021-01-01 16:35:47 +0900

    7. Nginx를 활용한 무중단 배포 구축하기 - real-application.yml 운영환경 설정 적용

---
+++ src/test/java/com/spring/webservice/web/WebRestControllerTest.java
@@ -0,0 +1,32 @@
+package com.spring.webservice.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class WebRestControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void test_profile () {
+        //when
+        String profile = this.restTemplate.getForObject("/profile", String.class);
+
+        //then
+        assertEquals(profile, "local");
+    }
+}
